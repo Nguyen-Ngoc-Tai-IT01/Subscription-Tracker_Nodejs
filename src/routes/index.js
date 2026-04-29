af1925f2db74df26edaa007f65f823055f42647d
NguyenNgocTai108742 <nguyenngoctai18062004@gmail.com>
2026-04-29 20:25:55 +0700

    29/04

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-// 1. Tuyển dụng 2 Lễ tân phụ
 const userRouter = require('./userRouter');
 const siteRouter = require('./siteRouter');
 
-// 2. Phân luồng công việc
 // Bất cứ link nào khách gõ, đưa cho cả 2 xem, ai xử lý được thì xử lý
 router.use('/', userRouter);
 router.use('/', siteRouter);
